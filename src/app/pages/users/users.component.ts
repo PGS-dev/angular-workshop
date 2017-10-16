14d2fd50a665cbd732f4460c24bc2385f4aed041
Marcin Orzechowski <morzechowski@pgs-soft.com>
2017-10-16 10:54:02 +0200

    read the firebase database from service

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -2,10 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MaterialModule } from '../../all-material.module';
 import { UsersService } from '../../services/users.service';
 
-import { Observable } from 'rxjs/Observable';
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database'; //AngularFireList v5
-import { AngularFireAuth } from 'angularfire2/auth';
-import * as firebase from 'firebase/app';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-users',
@@ -22,32 +19,15 @@ export class UsersComponent implements OnInit {
     ];*/
 
     dynamicUsers = [];
-    user: Observable<firebase.User>;
-    items: FirebaseListObservable<any[]>; //AngularFireList v5
-    msgVal: string = ''
+    dynamicUsersFirebase: FirebaseListObservable<any[]>;
 
-  constructor(private usersService:UsersService, public afAuth: AngularFireAuth, public af: AngularFireDatabase) {
-      this.items = af.list('users', {
-      query: {
-        limitToLast: 50
-      }
-    });
+  constructor(private usersService:UsersService) {
 
     //this.items.subscribe(console.log); //dana asynchroniczna! - petla bedzie pusta, chyba ze zastosujemy preserveSnapshot
-
-    //list all users
-    af.list('users', { preserveSnapshot: true})
-    .subscribe(snapshots=>{
-        snapshots.forEach(snapshot => {
-          console.log(snapshot.key, snapshot.val());
-        });
-    })
-
-    this.user = this.afAuth.authState;
   }
   ngOnInit() {
-      console.log(this.usersService.geUsers());
       this.dynamicUsers = this.usersService.geUsers();
+      this.dynamicUsersFirebase = this.usersService.geUsersFirebase();
   }
 
 }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -3,13 +3,45 @@ import { Injectable } from '@angular/core';
 import { UserModel } from './types';
 import { userList } from './mock-users';
 
+import { Observable } from 'rxjs/Observable';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database'; //AngularFireList v5
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+
 @Injectable()
 export class UsersService {
 
-  constructor() {}
+    user: Observable<firebase.User>;
+    items: FirebaseListObservable<any[]>; //AngularFireList v5
+    msgVal: string = ''
 
-  public geUsers(): UserModel[] {
-      return userList;
-  }
+    constructor(public afAuth: AngularFireAuth, public af: AngularFireDatabase) {
+
+      this.items = af.list('users', {
+      query: {
+        limitToLast: 50
+      }
+    });
+
+    //this.items.subscribe(console.log); //dana asynchroniczna! - petla bedzie pusta, chyba ze zastosujemy preserveSnapshot
+    //list all firebase users
+    //
+    af.list('users', { preserveSnapshot: true})
+    .subscribe(snapshots=>{
+        snapshots.forEach(snapshot => {
+          console.log(snapshot.key, snapshot.val());
+        });
+    })
+
+    this.user = this.afAuth.authState;
+    }
+
+    public geUsers(): UserModel[] {
+        return userList;
+    }
+
+    public geUsersFirebase(): FirebaseListObservable<any[]> {
+        return this.items;
+    }
 
 }
