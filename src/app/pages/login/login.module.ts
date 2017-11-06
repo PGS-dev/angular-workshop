99d3a0827ca46680b9368dc01f39de512f003d2d
Maxim Rachkovskiy <mrachkovskiy@pgs-soft.com>
2017-11-06 21:04:00 +0100

    login page styles

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material';
+import { MatFormFieldModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material';
+import { MatCardModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatCardModule
   ],
   declarations: [
     LoginComponent,
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { UsersModule } from './users/users.module';
 import { UserModule } from './user/user.module';
 import { LoginModule } from './login/login.module';
+
 @NgModule({
   imports: [
     CommonModule, UserModule, UsersModule, LoginModule
