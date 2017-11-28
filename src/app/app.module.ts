4228899d6210befda3dde19fe73de0f3d46835d4
Szymon Pawlak (PGS Software) <spawlak@pgs-soft.com>
2017-11-28 16:58:03 +0100

    test http request using SSR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
+
+// Components
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
@@ -16,7 +19,8 @@ import { HomeComponent } from './home/home.component';
       { path: '', component: HomeComponent, pathMatch: 'full'},
       { path: 'lazy', loadChildren: './lazy/lazy.module#LazyModule'},
       { path: 'lazy/nested', loadChildren: './lazy/lazy.module#LazyModule'}
-    ])
+    ]),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
+import {HttpClient} from '@angular/common/http';
+
 @Component({
   selector: 'home',
-  template: `<h3>{{ message }}</h3>`
+  template: `<h3>{{ data }}</h3>`
 })
 export class HomeComponent implements OnInit {
-  public message: string;
+  public data: any;
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.message = 'Hello';
+    this.http.get('https://qa.epoints.com/rest/search/redemptionitems?filters=s_categoryFromFeedExtractedSeoSlugs_multiVal:halloween,s_departmentSeoSlug:specials&page=1&pageSize=5').subscribe(data => {
+      // Read the result field from the JSON response.
+      this.data = data['pagesCount'];
+    });
   }
 }
