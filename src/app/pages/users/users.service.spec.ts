86955a8ce9ab3517b6e1f1e0970deef57a1c5cf3
whtrbit <kontakt@grzegorzbielak.pl>
2018-06-23 18:10:02 +0200

    Users.service different approach

--- src/app/pages/users/users.service.spec.ts
+++ src/app/pages/users/users.service.spec.ts
@@ -2,6 +2,7 @@ import {TestBed, inject} from '@angular/core/testing';
 import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
 import {UsersService} from './users.service';
 import {HttpClient} from "@angular/common/http";
+import {async} from "q";
 
 describe('UsersService', () => {
   let httpClient: HttpClient;
@@ -44,5 +45,31 @@ describe('UsersService', () => {
 
   afterEach(() => {
     httpTestingController.verify();
+  });
+});
+
+xdescribe('UsersService', () => {
+  let httpClientSpy: { get: jasmine.Spy };
+  let usersService: UsersService;
+
+  beforeEach(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    usersService = new UsersService(<any> httpClientSpy);
+  });
+
+  it('', () => {
+    const expectedUsers = [
+      { id: 0 },
+      { id: 1 }
+    ];
+
+    httpClientSpy.get.and.returnValue(async(expectedUsers));
+
+    usersService.getUsers().subscribe(
+      users => expect(users).toEqual(expectedUsers, 'expected users'),
+      fail
+    );
+
+    expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
   });
 });
